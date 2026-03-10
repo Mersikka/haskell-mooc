920535fb687f658e80156bfb91f25d42aa974088
mersikka <mio@argillander.me>
2026-03-11 00:29:23 +0200

    Complete Set9b

--- exercises/Set9b.hs
+++ exercises/Set9b.hs
@@ -47,10 +47,10 @@ type Col   = Int
 type Coord = (Row, Col)
 
 nextRow :: Coord -> Coord
-nextRow (i,j) = todo
+nextRow (i,j) = (i+1, 1)
 
 nextCol :: Coord -> Coord
-nextCol (i,j) = todo
+nextCol (i,j) = (i,j+1)
 
 --------------------------------------------------------------------------------
 -- Ex 2: Implement the function prettyPrint that, given the size of
@@ -103,7 +103,29 @@ nextCol (i,j) = todo
 type Size = Int
 
 prettyPrint :: Size -> [Coord] -> String
-prettyPrint = todo
+prettyPrint s cs = go s (1,1) (sortBy mySort cs) []
+  where
+    mySort (x1,y1) (x2,y2)
+      | x1 == x2 && y1 == y2 = EQ
+      | x1 == x2 && y1 < y2 = LT
+      | x1 == x2 && y1 > y2 = GT
+      | x1 < x2 = LT
+      | x1 > x2 = GT
+    go :: Size -> Coord -> [Coord] -> String -> String
+    go s c1 [] res
+      | fst c1 < s && snd c1 < s   = go s (nextCol c1) [] (res ++ ".")
+      | fst c1 == s && snd c1 == s = res ++ ".\n"
+      | fst c1 == s                = go s (nextCol c1) [] (res ++ ".")
+      | snd c1 == s                = go s (nextRow c1) [] (res ++ ".\n")
+    go s c1 (c2:cs) res
+      | fst c1 < s && snd c1 < s   = if c1 == c2 then go s (nextCol c1) cs (res ++ "Q")
+                                                 else go s (nextCol c1) (c2:cs) (res ++ ".")
+      | fst c1 == s && snd c1 == s = if c1 == c2 then res ++ "Q\n"
+                                                 else res ++ ".\n"
+      | fst c1 == s                = if c1 == c2 then go s (nextCol c1) cs (res ++ "Q")
+                                                 else go s (nextCol c1) (c2:cs) (res ++ ".")
+      | snd c1 == s                = if c1 == c2 then go s (nextRow c1) cs (res ++ "Q\n")
+                                                 else go s (nextRow c1) (c2:cs) (res ++ ".\n")
 
 --------------------------------------------------------------------------------
 -- Ex 3: The task in this exercise is to define the relations sameRow, sameCol,
@@ -127,16 +149,16 @@ prettyPrint = todo
 --   sameAntidiag (500,5) (5,500) ==> True
 
 sameRow :: Coord -> Coord -> Bool
-sameRow (i,j) (k,l) = todo
+sameRow (i,j) (k,l) = i == k
 
 sameCol :: Coord -> Coord -> Bool
-sameCol (i,j) (k,l) = todo
+sameCol (i,j) (k,l) = j == l
 
 sameDiag :: Coord -> Coord -> Bool
-sameDiag (i,j) (k,l) = todo
+sameDiag (i,j) (k,l) = i-k == j-l
 
 sameAntidiag :: Coord -> Coord -> Bool
-sameAntidiag (i,j) (k,l) = todo
+sameAntidiag (i,j) (k,l) = k-i == j-l
 
 --------------------------------------------------------------------------------
 -- Ex 4: In chess, a queen may capture another piece in the same row, column,
@@ -191,7 +213,7 @@ type Candidate = Coord
 type Stack     = [Coord]
 
 danger :: Candidate -> Stack -> Bool
-danger = todo
+danger c qs = any (\coord -> sameRow coord c || sameCol coord c || sameDiag coord c || sameAntidiag coord c) qs
 
 --------------------------------------------------------------------------------
 -- Ex 5: In this exercise, the task is to write a modified version of
@@ -226,7 +248,37 @@ danger = todo
 -- solution to this version. Any working solution is okay in this exercise.)
 
 prettyPrint2 :: Size -> Stack -> String
-prettyPrint2 = todo
+prettyPrint2 s cs = go s (1,1) (sortBy mySort cs) [] cs
+  where
+    mySort (x1,y1) (x2,y2)
+      | x1 == x2 && y1 == y2 = EQ
+      | x1 == x2 && y1 < y2 = LT
+      | x1 == x2 && y1 > y2 = GT
+      | x1 < x2 = LT
+      | x1 > x2 = GT
+    go :: Size -> Coord -> [Coord] -> String -> Stack -> String
+    go s c1 [] res qs
+      | fst c1 < s && snd c1 < s   = case danger c1 qs of False -> go s (nextCol c1) [] (res ++ ".") qs
+                                                          True  -> go s (nextCol c1) [] (res ++ "#") qs
+      | fst c1 == s && snd c1 == s = case danger c1 qs of False -> res ++ ".\n"
+                                                          True  -> res ++ "#\n"
+      | fst c1 == s                = case danger c1 qs of False -> go s (nextCol c1) [] (res ++ ".") qs
+                                                          True  -> go s (nextCol c1) [] (res ++ "#") qs
+      | snd c1 == s                = case danger c1 qs of False -> go s (nextRow c1) [] (res ++ ".\n") qs
+                                                          True  -> go s (nextRow c1) [] (res ++ "#\n") qs
+    go s c1 (c2:cs) res qs
+      | fst c1 < s && snd c1 < s   = if c1 == c2 then go s (nextCol c1) cs (res ++ "Q") qs
+                                                 else case danger c1 qs of False -> go s (nextCol c1) (c2:cs) (res ++ ".") qs
+                                                                           True  -> go s (nextCol c1) (c2:cs) (res ++ "#") qs
+      | fst c1 == s && snd c1 == s = if c1 == c2 then res ++ "Q\n"
+                                                 else case danger c1 qs of False -> res ++ ".\n"
+                                                                           True  -> res ++ "#\n"
+      | fst c1 == s                = if c1 == c2 then go s (nextCol c1) cs (res ++ "Q") qs
+                                                 else case danger c1 qs of False -> go s (nextCol c1) (c2:cs) (res ++ ".") qs
+                                                                           True  -> go s (nextCol c1) (c2:cs) (res ++ "#") qs
+      | snd c1 == s                = if c1 == c2 then go s (nextRow c1) cs (res ++ "Q\n") qs
+                                                 else case danger c1 qs of False -> go s (nextRow c1) (c2:cs) (res ++ ".\n") qs
+                                                                           True  -> go s (nextRow c1) (c2:cs) (res ++ "#\n") qs
 
 --------------------------------------------------------------------------------
 -- Ex 6: Now that we can check if a piece can be safely placed into a square in
@@ -271,7 +323,14 @@ prettyPrint2 = todo
 --     Q#######
 
 fixFirst :: Size -> Stack -> Maybe Stack
-fixFirst n s = todo
+fixFirst n (t@(r,c):qs)
+  | r > n || c > n = Nothing
+  | c == n         = if danger t qs then Nothing
+                                    else Just (t:qs)
+  | r == n         = if danger t qs then fixFirst n ((nextCol t):qs)
+                                    else Just (t:qs)
+  | otherwise      = if danger t qs then fixFirst n ((nextCol t):qs)
+                                    else Just (t:qs)
 
 --------------------------------------------------------------------------------
 -- Ex 7: We need two helper functions for stack management.
@@ -293,10 +352,10 @@ fixFirst n s = todo
 -- Hint: Remember nextRow and nextCol? Use them!
 
 continue :: Stack -> Stack
-continue s = todo
+continue (q:qs) = nextRow q:q:qs
 
 backtrack :: Stack -> Stack
-backtrack s = todo
+backtrack (_:q:qs) = nextCol q:qs
 
 --------------------------------------------------------------------------------
 -- Ex 8: Let's take a step. Our algorithm solves the problem (in a
@@ -365,7 +424,8 @@ backtrack s = todo
 --     step 8 [(6,1),(5,4),(4,2),(3,5),(2,3),(1,1)] ==> [(5,5),(4,2),(3,5),(2,3),(1,1)]
 
 step :: Size -> Stack -> Stack
-step = todo
+step n qs = case fixFirst n qs of Nothing   -> backtrack qs
+                                  (Just xs) -> continue xs
 
 --------------------------------------------------------------------------------
 -- Ex 9: Let's solve our puzzle! The function finish takes a partial
@@ -380,7 +440,9 @@ step = todo
 -- solve the n queens problem.
 
 finish :: Size -> Stack -> Stack
-finish = todo
+finish n qs@(_:xs)
+  | length qs == n+1 = xs
+  | otherwise        = finish n (step n qs)
 
 solve :: Size -> Stack
 solve n = finish n [(1,1)]
